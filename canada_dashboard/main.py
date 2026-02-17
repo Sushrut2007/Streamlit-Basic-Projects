ef7d166a59e1e7d1e0818074350ef7f752a1f085
Sushrut <sushrutdeshmukh4@gmail.com>
2026-02-17 12:30:51 +0530

    Implemented Compare tab to compare population trends with different locations

--- canada_dashboard/main.py
+++ canada_dashboard/main.py
@@ -37,7 +37,7 @@ with st.form("Canada form"):
     
     with col1:
         st.write('Choose a starting date')
-        start_Q = st.selectbox('Start quarter', options=sorted(df['Q'].unique()))
+        start_Q = st.selectbox('Start quarter', options=sorted(df['Q'].unique()), index=2)
         starting_year = st.slider(
             label = "Start Year", 
             min_value = df['Year'].min(),
@@ -49,7 +49,8 @@ with st.form("Canada form"):
         end_year = st.slider(
             label = "End year", 
             min_value = df['Year'].min(),
-            max_value = df['Year'].max())
+            max_value = df['Year'].max(),
+            value=1992)
         
     with col3:
         st.write('Choose a location')
@@ -58,46 +59,61 @@ with st.form("Canada form"):
     button = st.form_submit_button('Analyze')
 
 # Output section
-if button:
-    # Create mask targeting starting population
-    start_mask = (df['Q'] == start_Q) & (df['Year']==starting_year)
-    # Create mask targeting ending population
-    end_mask = (df['Q'] == end_Q) & (df['Year'] == end_year)
+
+# Create mask targeting starting population
+start_mask = (df['Q'] == start_Q) & (df['Year']==starting_year)
+# Create mask targeting ending population
+end_mask = (df['Q'] == end_Q) & (df['Year'] == end_year)
+
+# Error: the selected inputs does not match any of the rows
+if df[start_mask].empty or df[end_mask].empty:
+    e = IndexError("No data available. check your quarter and year selections")
+    st.error(e, icon="❌")
+# Error: start date > end date OR years are same and start quarter > end quarter
+elif (starting_year > end_year) or (starting_year == end_year and start_Q > end_Q):
+    st.error("Dates don't work. Start date must come before end date", icon = '⚠️')
+else: # Else display tabs 
+    tab1, tab2 = st.tabs(['Population change', 'Compare'])
+    # Tab 1 contents
+    with tab1:
+        st.header(f'Population change from {start_Q} {starting_year} to {end_Q} {end_year}')
+        col1, col2 = st.columns(2)
+        # Display initial and final population at selected years
+        with col1:
+            start_pop_value = df.loc[start_mask, location].iloc[0]
+            end_pop_value = df.loc[end_mask, location].iloc[0]
+            st.metric(f'{start_Q} {starting_year}', value=start_pop_value)
+            diff = (end_pop_value - start_pop_value) / start_pop_value * 100
+            st.metric(f'{end_Q} {end_year}', value=end_pop_value, delta=f'{diff:.2f}%')
+        # Plot the trend graph
+        with col2:
+            start_row = df[start_mask].index[0] # Retrieve the index of the start row
+            end_row = df[end_mask].index[0] # Retrieve the index of the start row
+            df_trend = df.iloc[min(start_row, end_row) : max(start_row, end_row) + 1]
+            fig, ax = plt.subplots()
+            ax.plot(df_trend['Quarter'], df_trend[location])
+            ax.set_xlabel('Time')
+            ax.set_ylabel('Population')
+            ax.set_xticks([df_trend['Quarter'].iloc[0], df_trend['Quarter'].iloc[-1]])
+            ax.get_yaxis().set_major_formatter( # Format the population to be included with ','
+                ticker.FuncFormatter(lambda x, p: format(int(x),',')))
+            st.pyplot(fig) # Plot the graph
     
-    # Error: the selected inputs does not match any of the rows
-    if df[start_mask].empty or df[end_mask].empty:
-        e = IndexError("No data available. check your quarter and year selections")
-        st.error(e, icon="❌")
-    # Error: start date > end date OR years are same and start quarter > end quarter
-    elif (starting_year > end_year) or (starting_year == end_year and start_Q > end_Q):
-        st.error("Dates don't work. Start date must come before end date", icon = '⚠️')
-
-    else: # Else display tabs 
-        tab1, tab2 = st.tabs(['Population change', 'Compare'])
-        # Tab 1 contents
-        with tab1:
-            st.header(f'Population change from {start_Q} {starting_year} to {end_Q} {end_year}')
-            col1, col2 = st.columns(2)
-
-            # Display initial and final population at selected years
-            with col1:
-                start_pop_value = df.loc[start_mask, location].iloc[0]
-                end_pop_value = df.loc[end_mask, location].iloc[0]
-                st.metric(f'{start_Q} {starting_year}', value=start_pop_value)
-
-                diff = (end_pop_value - start_pop_value) / start_pop_value * 100
-                st.metric(f'{end_Q} {end_year}', value=end_pop_value, delta=f'{diff:.2f}%')
-
-            # Plot the trend graph
-            with col2:
-                start_row = df[start_mask].index[0] # Retrieve the index of the start row
-                end_row = df[end_mask].index[0] # Retrieve the index of the start row
-                df_trend = df.iloc[min(start_row, end_row) : max(start_row, end_row) + 1]
-                fig, ax = plt.subplots()
-                ax.plot(df_trend['Quarter'], df_trend[location])
-                ax.set_xlabel('Time')
-                ax.set_ylabel('Population')
-                ax.set_xticks([df_trend['Quarter'].iloc[0], df_trend['Quarter'].iloc[-1]])
-                ax.get_yaxis().set_major_formatter( # Format the population to be included with ','
-                    ticker.FuncFormatter(lambda x, p: format(int(x),',')))
-                st.pyplot(fig) # Plot the graph
+    # Tab 2 contents
+    with tab2:
+        st.header('Compare with other locations')
+        locations = st.multiselect('Choose other locations',
+                        options=df.columns[1:-2], default= df.columns[1], max_selections=6)
+        
+        fig, ax = plt.subplots()
+        for place in locations:
+            ax.plot(df_trend['Quarter'], df_trend[place], label=place)
+        
+        if locations:
+            ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Population')
+        ax.set_xticks([df_trend['Quarter'].iloc[0], df_trend['Quarter'].iloc[-1]])
+        ax.get_yaxis().set_major_formatter( # Format the population to be included with ','
+            ticker.FuncFormatter(lambda x, p: format(int(x),',')))
+        st.pyplot(fig) # Plot the graph
