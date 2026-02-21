c2d2789640c9b81f07506efddeee92839740a570
Sushrut <sushrutdeshmukh4@gmail.com>
2026-02-21 22:02:25 +0530

    Modularized code and added st.cache_data for reading the dataset

--- canada_dashboard/main.py
+++ canada_dashboard/main.py
@@ -5,13 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker # For formatting the population
 
-# Get the directory where the current script is located
-dir_path = os.path.dirname(os.path.realpath(__file__))
-# Construct the path to the csv file relative to this folder
-path = os.path.join(dir_path, "data", "quarterly_canada_population.csv")
+@st.cache_data
+def read_data():
+    """Read the data file of the Canada population time series.
+       Also performs simple feature engineering for Quarter & Year columns.
+    Returns:
+        df: Dataframe containing the CSV file
+    """
+    # Get the directory where the current script is located
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # Construct the path to the csv file relative to this folder
+    path = os.path.join(dir_path, "data", "quarterly_canada_population.csv")
 
-
-df = pd.read_csv(path, dtype={'Quarter': str, 
+    df = pd.read_csv(path, dtype={'Quarter': str, 
                             'Canada': np.int32,
                             'Newfoundland and Labrador': np.int32,
                             'Prince Edward Island': np.int32,
@@ -27,57 +33,17 @@ df = pd.read_csv(path, dtype={'Quarter': str,
                             'Northwest Territories': np.int32,
                             'Nunavut': np.int32})
 
-st.title('Population of Canada')
+    # Extract the quarter (Q1, Q2, etc.) and year as a seperate columns
+    df[['Q', 'Year']] = df['Quarter'].str.split(expand=True)
+    df['Year'] = df['Year'].astype(int) # Convert year to integer
 
-with st.expander('See full table'):
-    st.write(df)
+    return df
 
-# Extract the quarter (Q1, Q2, etc.) and year as a seperate columns
-df[['Q', 'Year']] = df['Quarter'].str.split(expand=True)
-df['Year'] = df['Year'].astype(int) # Convert year to integer
+def display_dashboard(start_mask, end_mask, location):
+    """Display the Canada population dashboard.
+       Required correct input beforehand. 
+    """
 
-
-with st.form("Canada form"): 
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        st.write('Choose a starting date')
-        start_Q = st.selectbox('Start quarter', options=sorted(df['Q'].unique()), index=2)
-        starting_year = st.slider(
-            label = "Start Year", 
-            min_value = df['Year'].min(),
-            max_value = df['Year'].max())
-    
-    with col2:
-        st.write('Choose an end date')
-        end_Q = st.selectbox('End quarter', options=sorted(df['Q'].unique()))
-        end_year = st.slider(
-            label = "End year", 
-            min_value = df['Year'].min(),
-            max_value = df['Year'].max(),
-            value=1992)
-        
-    with col3:
-        st.write('Choose a location')
-        location = st.selectbox('Choose a location', options=df.columns[1:-2])
-
-    button = st.form_submit_button('Analyze')
-
-# Output section
-
-# Create mask targeting starting population
-start_mask = (df['Q'] == start_Q) & (df['Year']==starting_year)
-# Create mask targeting ending population
-end_mask = (df['Q'] == end_Q) & (df['Year'] == end_year)
-
-# Error: the selected inputs does not match any of the rows
-if df[start_mask].empty or df[end_mask].empty:
-    e = IndexError("No data available. check your quarter and year selections")
-    st.error(e, icon="❌")
-# Error: start date > end date OR years are same and start quarter > end quarter
-elif (starting_year > end_year) or (starting_year == end_year and start_Q > end_Q):
-    st.error("Dates don't work. Start date must come before end date", icon = '⚠️')
-else: # Else display tabs 
     tab1, tab2 = st.tabs(['Population change', 'Compare'])
     # Tab 1 contents
     with tab1:
@@ -122,3 +88,55 @@ else: # Else display tabs
         ax.get_yaxis().set_major_formatter( # Format the population to be included with ','
             ticker.FuncFormatter(lambda x, p: format(int(x),',')))
         st.pyplot(fig) # Plot the graph
+    
+
+if __name__ == '__main__':
+    df = read_data()
+
+    st.title('Population of Canada')
+
+    # Table view section
+    with st.expander('See full table'):
+        st.write(df)
+
+
+    with st.form("Canada form"): 
+        col1, col2, col3 = st.columns(3)
+    
+        with col1:
+            st.write('Choose a starting date')
+            start_Q = st.selectbox('Start quarter', options=sorted(df['Q'].unique()), index=2)
+            starting_year = st.slider(
+                label = "Start Year", 
+                min_value = df['Year'].min(),
+                max_value = df['Year'].max())
+
+        with col2:
+            st.write('Choose an end date')
+            end_Q = st.selectbox('End quarter', options=sorted(df['Q'].unique()))
+            end_year = st.slider(
+                label = "End year", 
+                min_value = df['Year'].min(),
+                max_value = df['Year'].max(),
+                value=1992)
+
+        with col3:
+            st.write('Choose a location')
+            location = st.selectbox('Choose a location', options=df.columns[1:-2])
+
+        button = st.form_submit_button('Analyze')
+
+    # Create mask targeting starting population
+    start_mask = (df['Q'] == start_Q) & (df['Year']==starting_year)
+    # Create mask targeting ending population
+    end_mask = (df['Q'] == end_Q) & (df['Year'] == end_year)
+
+    # Error: the selected inputs does not match any of the rows
+    if df[start_mask].empty or df[end_mask].empty:
+        e = IndexError("No data available. check your quarter and year selections")
+        st.error(e, icon="❌")
+    # Error: start date > end date OR years are same and start quarter > end quarter
+    elif (starting_year > end_year) or (starting_year == end_year and start_Q > end_Q):
+        st.error("Dates don't work. Start date must come before end date", icon = '⚠️')
+    else: # Else display tabs (dashboard)
+        display_dashboard(start_mask, end_mask, location) 
