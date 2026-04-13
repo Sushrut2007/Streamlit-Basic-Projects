3aef016c69b6909a53d87b38dbb478d19571f8d9
Sushrut <sushrutdeshmukh4@gmail.com>
2026-04-13 21:23:52 +0530

    Added model.joblib and implemented the entire project

--- car_price/main.py
+++ car_price/main.py
@@ -1,22 +1,46 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from joblib import load
 
+
 # Initialize the session state with an empty prediction
+if "predicted_price" not in st.session_state:
+    st.session_state['predicted_price'] = 0
+
 
 # Function to load the model (use caching)
+@st.cache_resource(show_spinner="Loading model...")
+def load_model(path):
+    model = load(path)
+
+    return model
 
 # Callback function to make a prediction.
 # Use kwargs to pass the model as an argument
 # It updates the value of the prediction stored in the state
 
-def main():
-    pass
+def make_prediction(_model):
+    # Transform input into a dataframe
+    data = {
+        'miles': [st.session_state.miles], 
+        'year': [st.session_state.year], 
+        'make': [st.session_state.make], 
+        'model': [st.session_state.model], 
+        'engine_size': [st.session_state.engine_size], 
+        'state': [st.session_state.province] }
+
+    input_df = pd.DataFrame(data)
+
+    st.session_state['predicted_price'] = round(_model.predict(input_df)[0], 1)
+
 
 if __name__ == "__main__":
     st.title("🍁Used car price calculator")
 
     # Load model
+    model = load_model("data/model/model.joblib")
+    
 
     with st.form(key="form"):
         col1, col2, col3 = st.columns(3)
@@ -39,10 +63,21 @@ if __name__ == "__main__":
             st.selectbox("Make", key="make", index=0, options=['toyota', 'honda'])
             st.selectbox("Province", index=0, key="province", options=['NB', 'QC', 'BC', 'ON', 'AB', 'MB', 'SK', 'NS', 'PE', 'NL', 'YT', 'NC', 'OH','SC'])
         
-        st.form_submit_button("Calculate", type="primary")
+        st.form_submit_button("Calculate", type="primary", on_click=make_prediction, kwargs={'_model': model})
 
     # Display the prediction
     # If the value is empty, display a message to click on the button
     # Otherwise, display the prediction
-    
+
+    if st.session_state.get('predicted_price') == 0:
+        st.header('Please click on Calculate button to make prediction')
+    else:
+        st.header(f"The estimated car price is {st.session_state.get('predicted_price')}$")
+
     st.write(st.session_state)
+    st.write(model.feature_names_in_)
+    st.write("Columns the regressor ACTUALLY used for math:")
+    st.write(model.steps[0][1].get_feature_names_out())
+
+
+
